d3faa66e0f96382e7d5fd240ad8355159d20c508
Akith Nimpura <mpakithn@gmail.com>
2024-06-30 15:32:35 +0530

    Changed Structure

---
+++ frontend/src/pages/garageownerpage/Garageowner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Garageowner = () => {
+  return (
+    <div>Garageowner</div>
+  )
+}
+
+export default Garageowner
---
+++ frontend/src/pages/vehicleownerpage/Vehicleowner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Vehicleowner = () => {
+  return (
+    <div>Vehicleowner</div>
+  )
+}
+
+export default Vehicleowner
