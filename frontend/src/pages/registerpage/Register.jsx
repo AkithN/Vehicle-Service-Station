fdf6cd57aa8586b039e28f31160d828ab0bffc4b
Diluka Theekshana <145269627+dilukatheekshana@users.noreply.github.com>
2024-08-23 22:28:00 +0530

    Register.jsx

--- frontend/src/pages/registerpage/Register.jsx
+++ frontend/src/pages/registerpage/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Input, Select, Checkbox, Radio, Button, Upload } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { Form, Input, Select, Checkbox, Radio, Button } from 'antd';
+import TextField from '@mui/material/TextField'; // Importing Material-UI's TextField
 import axios from 'axios';
 import './register.css';
 
@@ -48,9 +48,9 @@ function Register() {
 
   const [districts, setDistricts] = useState([]);
 
-  const handleImage = (info) => {
-    const { name } = info.file;
-    const file = info.file.originFileObj;
+  const handleImageChange = (event) => {
+    const { name } = event.target;
+    const file = event.target.files[0];
     if (file) {
       setImages((prevImages) => ({
         ...prevImages,
@@ -128,141 +128,218 @@ function Register() {
         onFinish={onSubmitHandler}
         layout="vertical"
       >
-        <Form.Item label="Name Of Service Center" name="garageName" rules={[{ required: true, message: 'Please enter the service center name' }]}>
+        <Form.Item
+          label="Name Of Service Center"
+          name="garageName"
+          rules={[{ required: true, message: 'Please enter the service center name' }]}
+        >
           <Input />
         </Form.Item>
-        <Form.Item label="Address" name="garageAddress" rules={[{ required: true, message: 'Please enter the address' }]}>
+        
+        <Form.Item
+          label="Address"
+          name="garageAddress"
+          rules={[{ required: true, message: 'Please enter the address' }]}
+        >
           <Input />
         </Form.Item>
-        <Form.Item label="Province" name="province" rules={[{ required: true, message: 'Please select a province' }]}>
+        
+        <Form.Item
+          label="Province"
+          name="province"
+          rules={[{ required: true, message: 'Please select a province' }]}
+        >
           <Select placeholder="--Select--">
             {Object.keys(provinceDistricts).map(province => (
               <Option key={province} value={province}>{province}</Option>
             ))}
           </Select>
         </Form.Item>
-        <Form.Item label="District" name="district" rules={[{ required: true, message: 'Please select a district' }]}>
+        
+        <Form.Item
+          label="District"
+          name="district"
+          rules={[{ required: true, message: 'Please select a district' }]}
+        >
           <Select placeholder="-- Select --">
             {districts.map(district => (
               <Option key={district} value={district}>{district}</Option>
             ))}
           </Select>
         </Form.Item>
-        <Form.Item label="Contact Number" name="workNum" rules={[{ required: true, message: 'Please enter a contact number' }]}>
+        
+        <Form.Item
+          label="Contact Number"
+          name="workNum"
+          rules={[{ required: true, message: 'Please enter a contact number' }]}
+        >
           <Input placeholder="Work" />
         </Form.Item>
-        <Form.Item label="Mobile Number" name="mobileNum" rules={[{ required: true, message: 'Please enter a contact number' }]}>
+        
+        <Form.Item
+          label="Mobile Number"
+          name="mobileNum"
+          rules={[{ required: true, message: 'Please enter a mobile number' }]}
+        >
           <Input placeholder="Mobile" />
         </Form.Item>
-        <div className='form-group'>
-          <Form.Item label="Types Of Services" name="services" rules={[{ required: true, message: 'Please select service' }]}>
-            <Checkbox.Group>
-              <Checkbox value="maintenance">Maintenance Services</Checkbox>
-              <Checkbox value="repair">Repair Services</Checkbox>
-              <Checkbox value="diagnostic">Diagnostic Services</Checkbox>
-              <Checkbox value="specialty">Specialty Services</Checkbox>
-              <Checkbox value="cosmetic">Cosmetic Services</Checkbox>
-              <Checkbox value="additional">Additional Services</Checkbox>
-              <Checkbox value="convenience">Convenience Services</Checkbox>
-            </Checkbox.Group>
-          </Form.Item>
-          <Form.Item label="Operating Hours (Weekdays)" name="operatingHoursWeek" rules={[{ required: true, message: 'Please select time' }]}>
-            <Radio.Group>
-              <Radio value="8-5">8.00 AM - 5.00 PM</Radio>
-              <Radio value="8-6">8.00 AM - 6.00 PM</Radio>
-              <Radio value="8-10">8.00 AM - 10.00 PM</Radio>
-              <Radio value="9-6">9.00 AM - 6.00 PM</Radio>
-              <Radio value="9-10">9.00 AM - 10.00 PM</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="Operating Hours (Weekends)" name="operatingHoursWeekend" rules={[{ required: true, message: 'Please select time' }]}>
-            <Radio.Group>
-              <Radio value="wclosed">Closed</Radio>
-              <Radio value="w9-3">9.00 AM - 3.00 PM</Radio>
-              <Radio value="w9-6">9.00 AM - 6.00 PM</Radio>
-              <Radio value="w9-10">9.00 AM - 10.00 PM</Radio>
-              <Radio value="w10-7">10.00 AM - 7.00 PM</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="Do You Work On Public Holidays?" name="publicHoliday" rules={[{ required: true, message: 'Please select' }]}>
-            <Radio.Group>
-              <Radio value="yes">Yes</Radio>
-              <Radio value="no">No</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="Availability Of Emergency Or 24/7 Service" name="emergencyService" rules={[{ required: true, message: 'Please select' }]}>
-            <Radio.Group>
-              <Radio value="yes">Yes</Radio>
-              <Radio value="no">No</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="Service Station Scale" name="serviceScale" rules={[{ required: true, message: 'Please select scale of service station' }]}>
-            <Radio.Group>
-              <Radio value="small">Small</Radio>
-              <Radio value="medium">Medium</Radio>
-              <Radio value="large">Large</Radio>
-            </Radio.Group>
-          </Form.Item>
-        </div>
-        <Form.Item label="Website" name="website" rules={[{ required: true, message: 'Please enter the website URL' }]}>
+        
+        <Form.Item
+          label="Types Of Services"
+          name="services"
+          rules={[{ required: true, message: 'Please select service' }]}
+        >
+          <Checkbox.Group>
+            <Checkbox value="maintenance">Maintenance Services</Checkbox>
+            <Checkbox value="repair">Repair Services</Checkbox>
+            <Checkbox value="diagnostic">Diagnostic Services</Checkbox>
+            <Checkbox value="specialty">Specialty Services</Checkbox>
+            <Checkbox value="cosmetic">Cosmetic Services</Checkbox>
+            <Checkbox value="additional">Additional Services</Checkbox>
+            <Checkbox value="convenience">Convenience Services</Checkbox>
+          </Checkbox.Group>
+        </Form.Item>
+        
+        <Form.Item
+          label="Operating Hours (Weekdays)"
+          name="operatingHoursWeek"
+          rules={[{ required: true, message: 'Please select time' }]}
+        >
+          <Radio.Group>
+            <Radio value="8-5">8.00 AM - 5.00 PM</Radio>
+            <Radio value="8-6">8.00 AM - 6.00 PM</Radio>
+            <Radio value="8-10">8.00 AM - 10.00 PM</Radio>
+            <Radio value="9-6">9.00 AM - 6.00 PM</Radio>
+            <Radio value="9-10">9.00 AM - 10.00 PM</Radio>
+          </Radio.Group>
+        </Form.Item>
+        
+        <Form.Item
+          label="Operating Hours (Weekends)"
+          name="operatingHoursWeekend"
+          rules={[{ required: true, message: 'Please select time' }]}
+        >
+          <Radio.Group>
+            <Radio value="wclosed">Closed</Radio>
+            <Radio value="w9-3">9.00 AM - 3.00 PM</Radio>
+            <Radio value="w9-6">9.00 AM - 6.00 PM</Radio>
+            <Radio value="w9-10">9.00 AM - 10.00 PM</Radio>
+            <Radio value="w10-7">10.00 AM - 7.00 PM</Radio>
+          </Radio.Group>
+        </Form.Item>
+        
+        <Form.Item
+          label="Do You Work On Public Holidays?"
+          name="publicHoliday"
+          rules={[{ required: true, message: 'Please select' }]}
+        >
+          <Radio.Group>
+            <Radio value="yes">Yes</Radio>
+            <Radio value="no">No</Radio>
+          </Radio.Group>
+        </Form.Item>
+        
+        <Form.Item
+          label="Availability Of Emergency Or 24/7 Service"
+          name="emergencyService"
+          rules={[{ required: true, message: 'Please select' }]}
+        >
+          <Radio.Group>
+            <Radio value="yes">Yes</Radio>
+            <Radio value="no">No</Radio>
+          </Radio.Group>
+        </Form.Item>
+        
+        <Form.Item
+          label="Service Station Scale"
+          name="serviceScale"
+          rules={[{ required: true, message: 'Please select scale of service station' }]}
+        >
+          <Select placeholder="--Select--">
+            <Option value="small">Small</Option>
+            <Option value="medium">Medium</Option>
+            <Option value="large">Large</Option>
+          </Select>
+        </Form.Item>
+        
+        <Form.Item
+          label="Description Of The Garage"
+          name="garageDescription"
+          rules={[{ required: true, message: 'Please enter description' }]}
+        >
+          <Input.TextArea rows={4} />
+        </Form.Item>
+        
+        <Form.Item
+          label="Website"
+          name="website"
+        >
           <Input />
         </Form.Item>
-        <Form.Item label="Facebook" name="facebook">
+        
+        <Form.Item
+          label="Facebook"
+          name="facebook"
+        >
           <Input />
         </Form.Item>
-        <Form.Item label="Instagram" name="instagram">
+        
+        <Form.Item
+          label="Instagram"
+          name="instagram"
+        >
           <Input />
         </Form.Item>
-        <Form.Item label="Twitter" name="twitter">
+        
+        <Form.Item
+          label="Twitter"
+          name="twitter"
+        >
           <Input />
         </Form.Item>
-        <Form.Item label="Service Station Description" name="garageDescription" rules={[{ required: true, message: 'Please provide a description' }]}>
-          <Input.TextArea />
-        </Form.Item>
+
         <div className='row'>
           <div className='column'>
             <Form.Item label="Upload Image 1">
-              <Upload
-                listType="picture"
-                maxCount={1}
-                beforeUpload={() => false}
-                onChange={handleImage}
+              <TextField
+                fullWidth
+                type="file"
                 name="image1"
-              >
-                <Button icon={<UploadOutlined />}>Upload Image 1</Button>
-              </Upload>
+                onChange={handleImageChange}
+                margin="normal"
+                inputProps={{ accept: 'image/*' }}
+              />
               {imagePreviews.image1 && <img src={imagePreviews.image1} alt="Preview" className="image-preview" />}
             </Form.Item>
           </div>
           <div className='column'>
             <Form.Item label="Upload Image 2">
-              <Upload
-                listType="picture"
-                maxCount={1}
-                beforeUpload={() => false}
-                onChange={handleImage}
+              <TextField
+                fullWidth
+                type="file"
                 name="image2"
-              >
-                <Button icon={<UploadOutlined />}>Upload Image 2</Button>
-              </Upload>
+                onChange={handleImageChange}
+                margin="normal"
+                inputProps={{ accept: 'image/*' }}
+              />
               {imagePreviews.image2 && <img src={imagePreviews.image2} alt="Preview" className="image-preview" />}
             </Form.Item>
           </div>
           <div className='column'>
             <Form.Item label="Upload Image 3">
-              <Upload
-                listType="picture"
-                maxCount={1}
-                beforeUpload={() => false}
-                onChange={handleImage}
+              <TextField
+                fullWidth
+                type="file"
                 name="image3"
-              >
-                <Button icon={<UploadOutlined />}>Upload Image 3</Button>
-              </Upload>
+                onChange={handleImageChange}
+                margin="normal"
+                inputProps={{ accept: 'image/*' }}
+              />
               {imagePreviews.image3 && <img src={imagePreviews.image3} alt="Preview" className="image-preview" />}
             </Form.Item>
           </div>
         </div>
+        
         <Form.Item>
           <Button type="primary" htmlType="submit">Submit</Button>
         </Form.Item>
