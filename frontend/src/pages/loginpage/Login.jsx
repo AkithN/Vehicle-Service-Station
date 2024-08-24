2968bc2e4966f1a116465e189b9b471e8d179125
OreshaW <MAHNDSE231F-016@student.nibm.lk>
2024-08-24 11:49:57 +0530

    login css

--- frontend/src/pages/loginpage/Login.jsx
+++ frontend/src/pages/loginpage/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./login.css";
+import bck from "../../assets/bck.jpg"; // Import background image
 
 const Login = () => {
   const [email, setEmail] = useState("");
